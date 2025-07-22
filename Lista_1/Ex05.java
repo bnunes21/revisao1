1633d9541c1343846528a77ceaa37216bceb36c1
Bruno <brunon004@gmail.com>
2025-07-22 14:13:38 -0300

    Enviando exercicio 1

--- Lista_1/Ex05.java
+++ Lista_1/Ex05.java
@@ -1,10 +1,19 @@
 package Lista_1;
 
+import java.util.Scanner;
+
 public class Ex05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+Scanner scanner = new Scanner (System.in);
+System.out.print("Informe um numero inteito");
+int numeroInteiro = scanner.nextInt();
 
+int antecessor = numeroInteiro-1;
+int sucessor = numeroInteiro+1;
+
+System.out.println(" O sucessor do numero " + numeroInteiro+ " É " +sucessor);
+System.out.println(" O antecessor do numero " + numeroInteiro+ " É " +antecessor);
 	}
 
 }
