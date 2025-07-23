a9d4735fac94d73c3047264ab792943b184b89fe
Bruno <brunon004@gmail.com>
2025-07-23 15:27:08 -0300

    Atualizando marca

--- Lista_1/Ex09.java
+++ Lista_1/Ex09.java
@@ -7,15 +7,19 @@ public class Ex09 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner (System.in);
 		
+		System.out.print("Informe a marca do carro: ");
+		String marca = scanner.nextLine();
 		System.out.print("Informe o modelo do carro: ");
 		String modelo = scanner.nextLine();
 		System.out.print("Informe o custo de fabricação: ");
 		double custo = scanner.nextDouble();
 		
+		
 		double calcDistribuidor = custo*0.28;
 		double calcImposto = custo*0.45;
 		
 		double total = calcDistribuidor+calcImposto+custo;
+		
 		
 		System.out.print("O valor total do modelo "+ modelo + " é de R$ "+ total);
 		
